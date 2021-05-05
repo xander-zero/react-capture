7f4e662da398b6b3ff40fcfaae3b8959c79b1758
xander-zero <mahmud.ansari60636@gmail.com>
2021-05-05 17:06:49 +0430

    4. Faq Section

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutSection from "../components/About/AboutSection";
+import FaqSection from "../components/FaqSection/FaqSection";
 import ServicesSection from "../components/ServiceSection/ServicesSection";
 
 const AboutUs = () => {
@@ -7,6 +8,7 @@ const AboutUs = () => {
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
