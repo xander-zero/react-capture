4d348f7b288ac1fbc07b6d33b4965731ab1d514b
xander-zero <mahmud.ansari60636@gmail.com>
2021-05-05 17:00:17 +0430

    3. Services Section

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutSection from "../components/About/AboutSection";
+import ServicesSection from "../components/ServiceSection/ServicesSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
+      <ServicesSection />
     </>
   );
 };
